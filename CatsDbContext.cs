2d49c7e40a99296b5b7e31c8fc57e3716d08dc9d
gcwolfe007 <gcwolfe007@gmail.com>
2022-06-27 17:21:46 -0600

    Add files via upload

---
+++ CatsDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CatsFuncs.models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CatsFuncs
+{
+   public class CatsDbContext : DbContext
+    {
+
+        public CatsDbContext(DbContextOptions<CatsDbContext> options)
+            : base(options)
+        { }
+        public DbSet<Cat> Cats { get; set; }
+    }
+}
